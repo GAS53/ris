62089bd31af5e2d5a14cda8774f5ff537943bfc6
Gleb Sviridoff <gas53@bk.ru>
2022-07-07 10:53:11 +0300

    pre news

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from django.views.generic import RedirectView
-import .views
+from .views import IndexTemplateView, WorksTemplateView, NewsTemplateView, WeTemplateView, GaleryTemplateView, ContactsTemplateView
+
 
 app_name = 'mainapp'
 
 
 urlpatterns = [
-        path('', RedirectView.as_view(url='index')),
-        path('index/', path(IndexTemplateView.as_view(), name='index'))
+        path('', IndexTemplateView.as_view(), name='index'),
+        path('works/', WorksTemplateView.as_view(), name='works'),
+        path('we/', WeTemplateView.as_view(), name='we'),
+        path('galery/', GaleryTemplateView.as_view(), name='galery'),
+        path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+        path('news/', ContactsTemplateView.as_view(), name='news'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,18 @@ from django.views.generic import TemplateView
 
 class IndexTemplateView(TemplateView):
     template_name = 'mainapp/index.html'
+
+class WeTemplateView(TemplateView):
+    template_name = 'mainapp/we.html'
+
+class GaleryTemplateView(TemplateView):
+    template_name = 'mainapp/galery.html'
+
+class ContactsTemplateView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+class WorksTemplateView(TemplateView):
+    template_name = 'mainapp/works.html'
+
+class NewsTemplateView(TemplateView):
+    template_name = 'mainapp/news.html'
